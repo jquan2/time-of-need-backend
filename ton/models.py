3d170956ec441ab209e3625b050ef362d9c8aa7e
tjslauson <tjslauson@alaska.edu>
2016-03-26 10:55:49 -0800

    Move tables around (no functional change)

--- ton/models.py
+++ ton/models.py
@@ -4,6 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Authorization tables
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
 
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
@@ -14,24 +21,22 @@ class Role(db.Model, RoleMixin):
         return self.name
 
 
-class Service(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(40), unique=True)
-    description = db.Column(db.String(255))
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
     def __str__(self):
-        return self.name
+        return self.email
 
 
-roles_users = db.Table(
-    'roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
-)
-
-
-services_locations = db.Table(
-    'services_locations',
+# Data tables
+locations_services = db.Table(
+    'locations_services',
     db.Column('location_id', db.Integer(), db.ForeignKey('location.id')),
     db.Column('service_id', db.Integer(), db.ForeignKey('service.id'))
 )
@@ -49,6 +54,15 @@ locations_days_of_week = db.Table(
     db.Column('location_id', db.Integer(), db.ForeignKey('location.id')),
     db.Column('day_of_week_id', db.Integer(), db.ForeignKey('day_of_week.id'))
 )
+
+
+class Service(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(40), unique=True)
+    description = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
 
 
 class DayOfWeek(db.Model):
@@ -79,19 +93,6 @@ class Zipcode(db.Model):
         return self.zip
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
-
-    def __str__(self):
-        return self.email
-
-
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
@@ -99,10 +100,11 @@ class Location(db.Model):
     phone = db.Column(db.String(30))
     contact_email = db.Column(db.String(256))
     website = db.Column(db.String(256))
-    services = db.relationship('Service', secondary=services_locations,
-                               backref=db.backref('locations', lazy='dynamic'))
     opening_time = db.Column(db.Time())
     closing_time = db.Column(db.Time())
+    services = db.relationship(
+        'Service', secondary=locations_services,
+        backref=db.backref('locations', lazy='dynamic'))
     zip_codes = db.relationship(
         'Zipcode', secondary=locations_zipcodes,
         backref=db.backref('locations', lazy='dynamic'))
