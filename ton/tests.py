3a74a180d4055b47e1837515fda00d120c6c5abe
tjslauson <tjslauson@alaska.edu>
2016-04-11 15:50:46 -0800

    Lint tests file

--- ton/tests.py
+++ ton/tests.py
@@ -65,10 +65,9 @@ class ApiTests(TonTests):
         self.assertEqual(j['locations'][0][key], expected)
 
     def validate_api_empty_string_field(self, json_field):
-        loc = self.create_location()
+        self.create_location()
         j = self.get_json_as_dict()
         self.assertNotIn(json_field, j['locations'][0])
-
 
     # Tests
     def test_location_name(self):
@@ -139,39 +138,37 @@ class ApiTests(TonTests):
 
     def test_location_empty_description(self):
         self.validate_api_empty_string_field("description")
-    
+
     def test_location_empty_address_line1(self):
         self.validate_api_empty_string_field("address_line1")
 
     def test_location_empty_address_line2(self):
         self.validate_api_empty_string_field("address_line2")
-    
+
     def test_location_empty_address_line3(self):
         self.validate_api_empty_string_field("address_line3")
-    
+
     def test_location_empty_phone(self):
         self.validate_api_empty_string_field("phone")
-    
+
     def test_location_empty_contact_email(self):
         self.validate_api_empty_string_field("contact_email")
-    
+
     def test_location_empty_website(self):
         self.validate_api_empty_string_field("website")
-    
+
     def test_location_empty_opening_time(self):
         self.validate_api_empty_string_field("opening_time")
-    
+
     def test_location_empty_closing_time(self):
         self.validate_api_empty_string_field("closing time")
-    
+
     def test_location_empty_days_of_week(self):
         db.session.add(DayOfWeek(day="FooDay"))
         db.session.add(DayOfWeek(day="FooDat"))
         self.validate_api_empty_string_field("days_of_week")
-    
+
     def test_location_empty_services(self):
         db.session.add(Service(name="BarService"))
         db.session.add(Service(name="FooService"))
         self.validate_api_empty_string_field("services")
-    
-
